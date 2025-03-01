c7878ca2e7ad48078bd3862b2d9d463887a17aa9
Valeriia Fisenko <valeri@Valeriias-MBP.home>
2025-03-01 18:53:41 +0100

    Initial commit

---
+++ infinitely_scrollable_image_grid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  infinitely_scrollable_image_grid
+//
+//  Created by Valeriia Fisenko on 01/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
