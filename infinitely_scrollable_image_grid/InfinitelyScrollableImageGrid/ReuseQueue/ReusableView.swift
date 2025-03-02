e045a95186adb77b7a80c61a264ae4f59eaf10cf
Valeriia Fisenko <valeri@Valeriias-MBP.home>
2025-03-02 20:40:27 +0100

    Implement ReusableQueue

---
+++ infinitely_scrollable_image_grid/InfinitelyScrollableImageGrid/ReuseQueue/ReusableView.swift
@@ -0,0 +1,11 @@
+//
+//  ReusableView.swift
+//  infinitely_scrollable_image_grid
+//
+//  Created by Valeriia Fisenko on 02/03/2025.
+//
+
+
+protocol ReusableView {
+    func prepareForReuse()
+}
---
+++ infinitely_scrollable_image_grid/InfinitelyScrollableImageGrid/ReuseQueue/ReuseQueue.swift
@@ -0,0 +1,54 @@
+//
+//  ReuseQueue.swift
+//  infinitely_scrollable_image_grid
+//
+//  Created by Valeriia Fisenko on 02/03/2025.
+//
+
+import UIKit
+
+final class ReuseQueue {
+
+    private var reusePool: [String: [UIView]] = [:]
+    private var presentingViews: [String: [UIView]] = [:]
+
+    func createView(_ view: UIView, withIdentifier identifier: String) {
+        presentingViews[identifier, default: []].append(view)
+    }
+
+    func removeAll(withIdentifier identifier: String) {
+        for view in (presentingViews[identifier] ?? []) {
+            view.removeFromSuperview()
+        }
+        presentingViews[identifier] = []
+//        reusePool[identifier] = []
+    }
+
+    func allocatedViews(withIdentifier identifier: String) -> [UIView] {
+        presentingViews[identifier] ?? []
+    }
+
+    func dequeueReusableView(withIdentifier identifier: String) -> UIView? {
+        guard var views = reusePool[identifier], views.isEmpty == false else {
+            return nil
+        }
+
+        let view = views.removeLast()
+        reusePool[identifier] = views
+        return view
+    }
+
+    func enqueueReusableView(_ view: UIView, withIdentifier identifier: String) {
+        if let reusableView = view as? ReusableView {
+            reusableView.prepareForReuse()
+        }
+
+        reusePool[identifier, default: []].append(view)
+
+        var allocated = presentingViews[identifier, default: []]
+        if let index = allocated.firstIndex(of: view) {
+            allocated.remove(at: index)
+            presentingViews[identifier] = allocated
+        }
+    }
+}
