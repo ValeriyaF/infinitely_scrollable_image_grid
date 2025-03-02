ee37f739202faedaa4df0bbb7737ba3138c3f189
Valeriia Fisenko <valeri@Valeriias-MBP.home>
2025-03-02 19:31:41 +0100

    Implement image loader

---
+++ infinitely_scrollable_image_grid/ImageLoader/ImageLoader.swift
@@ -0,0 +1,62 @@
+//
+//  ImageLoader.swift
+//  infinitely_scrollable_image_grid
+//
+//  Created by Valeriia Fisenko on 02/03/2025.
+//
+
+import UIKit
+
+enum ImageLoaderErrors: Error {
+    case cannotLoadImage
+    case cannotDecodeImage
+}
+
+protocol ImageLoader {
+    func loadRandomImage(for key: String, size: Int) async throws -> UIImage
+}
+
+final class ImageLoaderImpl: ImageLoader {
+
+    private var imageCache = NSCache<NSString, PicsumImage>()
+
+    func loadRandomImage(for key: String, size: Int) async throws -> UIImage {
+        let nsKey = key as NSString
+        let cachedImage: PicsumImage? = imageCache.object(forKey: nsKey)
+
+        if let cachedImage, cachedImage.image.size.width >= CGFloat(size) {
+            return cachedImage.image
+        }
+
+        let image = try await loadImage(byId: cachedImage?.id, size: size)
+        imageCache.setObject(image, forKey: nsKey)
+
+        return image.image
+    }
+}
+
+extension ImageLoaderImpl {
+
+    private func loadImage(byId id: String? = nil, size: Int) async throws -> PicsumImage {
+        guard let url = buildURL(id: id, size: size) else { throw ImageLoaderErrors.cannotLoadImage }
+
+        let (data, urlResp) = try await URLSession.shared.data(from: url)
+        guard let image = UIImage(data: data) else { throw ImageLoaderErrors.cannotDecodeImage  }
+        let imageId = (urlResp as? HTTPURLResponse)?.value(forHTTPHeaderField: "picsum-id")
+
+        return PicsumImage(id: imageId, image: image)
+    }
+
+    func buildURL(id: String?, size: Int) -> URL? {
+        guard var url = URL(string: "https://picsum.photos") else { return nil }
+
+        if let id = id {
+            url.appendPathComponent("id")
+            url.appendPathComponent("\(id)")
+        }
+
+        url.appendPathComponent("\(size)")
+
+        return url
+    }
+}
---
+++ infinitely_scrollable_image_grid/ImageLoader/PicsumImage.swift
@@ -0,0 +1,19 @@
+//
+//  PicsumImage.swift
+//  infinitely_scrollable_image_grid
+//
+//  Created by Valeriia Fisenko on 02/03/2025.
+//
+
+import UIKit
+
+// NSCache requires reference semantics
+final class PicsumImage {
+    let id: String?
+    let image: UIImage
+
+    init(id: String?, image: UIImage) {
+        self.id = id
+        self.image = image
+    }
+}
